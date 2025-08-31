dcc13c1c53b994c7d0bd2c7a5556c4ee9ef9fcc9
John <128350391+alpaOo@users.noreply.github.com>
2025-08-31 13:50:01 +0800

    chore: initialize vite

---
+++ client/src/main.ts
@@ -0,0 +1,50 @@
+import './style.css'
+
+const messages = document.getElementById("messages");
+
+const ws = new WebSocket("ws://localhost:8080/ws");
+const messageInput = document.getElementById("messageInput");
+
+messageInput.addEventListener("keydown", (event) => {
+	if (event.key === "Enter") {
+		if (!messageInput.value) {
+			return;
+		}
+
+		ws.send(messageInput.value);
+		// appendMessage(messages, "client", messageInput.value);
+		// This is a bad approach. We want to append the message to message window
+		// only if the message was sent successfully.
+		console.log(`[Client] ${messageInput.value}`);
+		messageInput.value = "";
+	}
+});
+
+function appendMessage(parent, source, message) {
+	const li = document.createElement("li");
+	li.textContent = message;
+
+	if (source === "server") {
+		li.className = "received-message";
+	} else if (source === "client") {
+		li.className = "sent-message";
+	}
+	parent.appendChild(li);
+}
+
+ws.onopen = () => {
+	console.log("Connected to server");
+};
+
+ws.onmessage = (event) => {
+	console.log(`[Server] ${event.data}`);
+	appendMessage(messages, "server", event.data);
+};
+
+ws.onclose = (event) => {
+	console.log(`[Connection closed] ${event.reason}`);
+};
+
+ws.onerror = (error) => {
+	console.log(`[WebSocket error] ${error}`);
+};
